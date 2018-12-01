4a0da4f9d0d2ec7802b14b1a6b08920c2e511243
eraute200 <emily_rautenberg@comcast.com>
2018-12-01 23:05:06 +0000

    Basic project set up

---
+++ public/navigation.js
@@ -0,0 +1,11 @@
+// NAVIGATION
+const days = 25;
+var nav = document.getElementById('nav');
+for (let i=1; i<=days; i++) {
+  let list = document.createElement('li');
+  let link = document.createElement('a');
+  link.href = `./${i}.html`;
+  link.innerHTML = `${i}`;
+  list.appendChild(link);
+  nav.appendChild(list);
+}
